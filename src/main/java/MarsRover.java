9df28b434505362f6a8363d772f8be2292154e05
Deepana Naidu <deepana.naidu@gmail.com>
2022-04-10 22:31:27 +0100

    Added method roverInstruction that will execute instructions sent to it

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,2 +1,19 @@
 public class MarsRover {
+
+    private int x;
+    private int y;
+    private String directionRoverFacing;
+
+    public MarsRover(int x, int y, String directionRoverFacing) {
+        this.x = x;
+        this.y = y;
+        this.directionRoverFacing = directionRoverFacing;
+    }
+
+
+    public String roverInstruction(String instruction) {
+        String originalPosition = String.valueOf(x) + " " + String.valueOf(y) + " " + directionRoverFacing;
+        String finalPosition = "";
+        return instruction.equals("") ? originalPosition : finalPosition;
+    }
 }

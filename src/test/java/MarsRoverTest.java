82b6eb7478319d7661ac07cbcc4384d6c1a004ec
Deepana Naidu <deepana.naidu@gmail.com>
2022-04-10 23:17:12 +0100

    Added checkPositionWhenInstructionMFacingS test

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,26 +1,36 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
-    private MarsRover marsRoverObject;
-
-    @BeforeEach
-    void setUp() {
-        marsRoverObject = new MarsRover(1, 2, "N");
-    }
 
     @Test
     public void checkPositionWhenInstructionIsEmpty(){
+        MarsRover marsRoverObject =  new MarsRover(1, 2, "N");
         String instruction = "";
         assertEquals("1 2 N",marsRoverObject.roverInstruction(instruction));
     }
 
     @Test
-    public void checkPositionWhenInstructionIsM(){
+    public void checkPositionWhenInstructionIsMRoverFaceN(){
+        MarsRover marsRoverObject =  new MarsRover(1, 2, "N");
         String instruction = "M";
         assertEquals("1 3 N",marsRoverObject.roverInstruction(instruction));
     }
+
+    @Test
+    public void checkPositionWhenInstructionIsMMRoverFaceN(){
+        MarsRover marsRoverObject =  new MarsRover(1, 2, "N");
+        String instruction = "MM";
+        assertEquals("1 4 N",marsRoverObject.roverInstruction(instruction));
+    }
+
+    @Test
+    public void checkPositionWhenInstructionIsMRoverFaceS(){
+        MarsRover marsRoverObject =  new MarsRover(1, 2, "S");
+        String instruction = "M";
+        assertEquals("1 1 S",marsRoverObject.roverInstruction(instruction));
+    }
+
 
 }

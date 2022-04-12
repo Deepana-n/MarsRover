8dbd847c91ba533941ce548f5c94a710e8737730
Deepana Naidu <deepana.naidu@gmail.com>
2022-04-12 21:03:25 +0100

    Removed duplicate method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,39 +6,26 @@ public class Main {
         System.out.println("Please enter size of Plateau");
         Scanner sc = new Scanner(System.in);
         String plateauSize = sc.nextLine();
+        plateauSize = plateauSize.replaceAll("\\s+","");
         Plateau plateau = new Plateau(Integer.parseInt(String.valueOf(plateauSize.charAt(0))),Integer.parseInt(String.valueOf(plateauSize.charAt(1))));
-        String rover1FinalPosition = rover1FinalPosition(sc, plateau);
-        String rover2FinalPosition = rover2FinalPosition(sc, plateau);
-        System.out.println("Your rover1 is at position---->" + rover1FinalPosition);
-        System.out.println("Your rover2 is at position---->" + rover2FinalPosition);
+        String rover1FinalPosition = roverFinalPosition(sc, plateau);
+        String rover2FinalPosition = roverFinalPosition(sc, plateau);
+        System.out.println("Your rover1's final position ----> " + rover1FinalPosition);
+        System.out.println("Your rover2's final position ---->" + rover2FinalPosition);
         setJFrame(plateau,rover1FinalPosition,rover2FinalPosition);
     }
 
-    private static String rover1FinalPosition(Scanner sc, Plateau plateau){
-        System.out.println("Please enter original coordinates of Rover 1");
+    private static String roverFinalPosition(Scanner sc, Plateau plateau){
+        System.out.println("Please enter starting coordinates of Rover");
         String originalCoordinates = sc.nextLine();
         originalCoordinates = originalCoordinates.replaceAll("\\s+","");
         int x = Integer.parseInt(String.valueOf(originalCoordinates.charAt(0)));
         int y = Integer.parseInt(String.valueOf(originalCoordinates.charAt(1)));
         String direction = String.valueOf(originalCoordinates.charAt(2));
-        System.out.println("Please enter instructions for Rover");
+        System.out.println("Please enter instructions to move Rover");
         String instructions = sc.nextLine();
-        MarsRover rover1 = new MarsRover(plateau,new Position(x,y,direction));
-
-        return rover1.moveRover(instructions);
-    }
-
-    private static String rover2FinalPosition(Scanner sc, Plateau plateau){
-        System.out.println("Please enter original coordinates of Rover 2");
-        String originalCoordinates2 = sc.nextLine();
-        originalCoordinates2 = originalCoordinates2.replaceAll("\\s+","");
-        int x2 = Integer.parseInt(String.valueOf(originalCoordinates2.charAt(0)));
-        int y2 = Integer.parseInt(String.valueOf(originalCoordinates2.charAt(1)));
-        String direction2 = String.valueOf(originalCoordinates2.charAt(2));
-        System.out.println("Please enter instructions for Rover 2");
-        String instructions2 = sc.nextLine();
-        MarsRover rover2 = new MarsRover(plateau,new Position(x2,y2,direction2));
-        return rover2.moveRover(instructions2);
+        MarsRover rover = new MarsRover(plateau,new Position(x,y,direction));
+        return rover.moveRover(instructions);
     }
 
     private static void setJFrame(Plateau plateau, String rover1FinalPosition, String rover2FinalPosition){
